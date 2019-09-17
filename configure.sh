afa5ac8685f7e6803689a87f63df568ba2e82d64
Samuel Farrens <samuel.farrens@gmail.com>
2019-09-17 10:44:59 +0200

    updated

--- configure.sh
+++ configure.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
-echo "Please specify your package name:"
+echo "Please provide the following information:"
 
-read -p "> " _package_name_
+read -p "> Your name: " _author_
+read -p "> Your email address: " _email_
+read -p "> The current year [2019]: " _year_
+read -p "> Your package name: " _package_name_
+read -p "> Your package GitHub address: " _url_
+
+_year_=${_year_:-"2019"}
 
 echo
 
-echo "'configure_package_name' will be updated to '$_package_name_' in the following files:"
+echo "'configure_package_name' will be updated to '$_package_name_' in the following files and directories:"
 
-ls ./package_name
+echo ./package_name
 grep -rl "configure_package_name" . --exclude=*.pyc --exclude=*.git* --exclude=configure.sh
 
 echo
 
-echo "Are you sure you want to update these files to $_package_name_?"
+echo "And the package will be updated with the following details:"
+echo " - Your name: $_author_"
+echo " - Your email_address: $_email_"
+echo " - The current year: $_year_"
+echo " - Your package GitHub address: $_url_"
+
+echo
+
+echo "Are you sure you want to make these changes?"
 
 read -p "> [y/N]: " _response_
 
@@ -22,10 +36,21 @@ echo
 _response_=${_response_:-"n"}
 
 if [ "$_response_" != "Y"  ] && [ "$_response_" != "y"  ]; then
-  echo "Better start over!"
+  echo "Looks like you are not happy with that. Better start over!"
   exit 1
 fi
 
+update_template() {
+  grep -rl $1 . --exclude=*.pyc --exclude=*.git* --exclude=configure.sh| xargs sed -i '' -e "s/$1/$2/g"
+}
+
 export LC_ALL=C
 mv ./package_name ./$_package_name_
-grep -rl "configure_package_name" . --exclude=*.pyc --exclude=*.git* --exclude=configure.sh| xargs sed -i '' -e "s/configure_package_name/$_package_name_/g"
+update_template "configure_package_name" $_package_name_
+update_template "configure_author" $_author_
+update_template "configure_email" $_email_
+update_template "configure_year" $_year_
+update_template "configure_url" $_url_
+
+
+# grep -rl "configure_package_name" . --exclude=*.pyc --exclude=*.git* --exclude=configure.sh| xargs sed -i '' -e "s/configure_package_name/$_package_name_/g"
