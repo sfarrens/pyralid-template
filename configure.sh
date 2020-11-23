d6fde83f88236b122b5e6f0c5a4e9563441623c1
Samuel Farrens <samuel.farrens@gmail.com>
2020-11-23 17:26:24 +0100

    added requirements to configure script

--- configure.sh
+++ configure.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
+# Divider line
+line="########################################################################"
+version="0.0.2"
+
+echo
+echo $line
+echo "Python Package Set Up Script"
+echo
+echo "Author: Samuel Farrens"
+echo "Year: 2020"
+echo "Version:" $version
+echo
+echo "See https://github.com/sfarrens/python_package_template for help."
+echo
+echo $line
+echo
 echo "Please provide the following information:"
 
 read -p "> Your name: " _author_
 read -p "> Your email address: " _email_
-read -p "> The current year [2019]: " _year_
+read -p "> The current year [2020]: " _year_
 read -p "> Your package name: " _package_name_
 read -p "> Your package GitHub address: " _url_
 read -p "> A short description of your package: " _describe_
+read -p "> A list of packages your code requires (e.g. numpy,scipy,matplotlib): " _requires_
 
-_year_=${_year_:-"2019"}
+
+_year_=${_year_:-"2020"}
 
 echo
 
@@ -26,6 +44,7 @@ echo " - Your email_address: $_email_"
 echo " - The current year: $_year_"
 echo " - Your package GitHub address: $_url_"
 echo " - A short description of your package: $_describe_"
+echo " - A list of packages your code requires: $_requires_"
 
 echo
 
@@ -50,6 +69,13 @@ update_template() {
   fi
 }
 
+update_requirements() {
+  for package in $(echo $_requires_ | sed "s/,/ /g")
+  do
+    echo $package >> requirements.txt
+  done
+}
+
 export LC_ALL=C
 mv ./package_name ./$_package_name_
 update_template "configure_package_name" "$_package_name_"
@@ -58,6 +84,7 @@ update_template "configure_email" "$_email_"
 update_template "configure_year" "$_year_"
 update_template "configure_url" "$_url_"
 update_template "configure_description" "$_describe_"
+update_requirements
 
 finish() {
   echo "All done!"
